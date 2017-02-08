d7b160f7dad84308297db25f649f62ce5395648a
Naoghuman <peter.rogge@yahoo.de>
2017-02-08 19:57:13 +0100

    [SimpleJsonReader] reads now the file [releases.json] from [GitHub] project.

--- src/main/java/com/github/naoghuman/abclist/json/SimpleJsonReader.java
+++ src/main/java/com/github/naoghuman/abclist/json/SimpleJsonReader.java
@@ -18,11 +18,17 @@ package com.github.naoghuman.abclist.json;
 
 import com.github.naoghuman.abclist.configuration.IJSONConfiguration;
 import com.github.naoghuman.lib.logger.api.LoggerFacade;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -45,7 +51,24 @@ public class SimpleJsonReader implements IJSONConfiguration {
         final JSONParser parser = new JSONParser();
         final Project project = new Project();
         try {
-            final Object object = parser.parse(new FileReader("release\\releases.json")); // NOI18N
+            String fileName = "https://raw.githubusercontent.com/Naoghuman/ABC-List/master/release/releases.json"; // NOI18N
+            URL url = new URL(fileName);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            Map<String, List<String>> headerMap = httpURLConnection.getHeaderFields();
+            
+            // If URL is getting 301 and 302 redirection HTTP code then get new URL link.
+            // This below for loop is totally optional if you are sure that your URL is not getting redirected to anywhere
+            for (String header : headerMap.get(null)) {
+                if (header.contains(" 302 ") || header.contains(" 301 ")) {
+                    fileName = headerMap.get("Location").get(0);
+                    url = new URL(fileName);
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    headerMap = httpURLConnection.getHeaderFields();
+                }
+            }
+            
+            final InputStream inputStream = httpURLConnection.getInputStream();
+            final Object object = parser.parse(new BufferedReader(new InputStreamReader(inputStream))); // NOI18N
             final JSONObject jsonObject = (JSONObject) object;
 //            project.setAboutURL(String.valueOf(jsonObject.get(KEY__ABOUT_URL)));
 //            project.setLicenseURL(String.valueOf(jsonObject.get(KEY__LICENSE_URL)));
